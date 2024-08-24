e21e5e333838d8408b9d186f1715bc5cd73c74eb
AlbertRossJoh <albertrossj@gmail.com>
2024-08-24 18:10:40 +0200

    Update Record9999Tests.cs

--- TaxCardTests/Record9999Tests.cs
+++ TaxCardTests/Record9999Tests.cs
@@ -1,3 +1,4 @@
+using TaxCardFormat.Enums;
 using Xunit;
 using TaxCardTests.Helpers;
 using TaxCardFormat.Records;
@@ -18,6 +19,42 @@ namespace TaxCardTests
             // Act
             var resultString = Sut.BuildString();
             
+            // Assert
+            HelpersAssert.RangeEquals(
+                _fieldRanges.FieldNameToRange[nameof(Record9999.Lb_nr)], 
+                Padding.ZeroPad(expectedLbNr, 7),
+                resultString
+            );
+            HelpersAssert.RangeEquals(
+                _fieldRanges.FieldNameToRange[nameof(Record9999.Rec_nr)],
+                expectedRecNr.ToString(),
+                resultString
+            );
+            HelpersAssert.RangeEquals(
+                _fieldRanges.FieldNameToRange[nameof(Record9999.AntalRecords)], 
+                Padding.ZeroPad(expectedAntalRecords, 7), 
+                resultString
+            );
+        }
+
+        [Fact]
+        public void AddRecord9999_ShouldHaveCorrectLbNr()
+        {
+            // Arrange
+            var i = 0;
+            for (; i < 9999; i++)
+            {
+                Sut.AddRecord6001((decimal)Random.Shared.NextDouble(), FeltNummer.AmBidrag);
+            }
+            Sut.AddRecord9999();
+            var expectedLbNr = ++i;
+            var expectedRecNr = 9999;
+            var expectedAntalRecords = ++i;
+            
+            
+            // Act
+            var result = Sut.BuildString();
+            var resultString = result.Split()[^2];
             // Assert
             HelpersAssert.RangeEquals(
                 _fieldRanges.FieldNameToRange[nameof(Record9999.Lb_nr)], 
